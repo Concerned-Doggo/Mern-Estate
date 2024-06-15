669b8df87c33683928963630f017f04201ee0e99
concerned-doggo <codeformetoo@proton.me>
2024-06-15 13:26:35 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRouter from './routes/user.route.js'
+
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -15,9 +17,12 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 
 app.use('/api/user', userRouter);
+
+app.use('/api/auth', authRouter);
 
 
 const port = 3000;
