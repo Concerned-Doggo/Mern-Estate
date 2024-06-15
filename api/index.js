1cf271b501d8c520d9eaccec567c33e29a6ea9b4
concerned-doggo <codeformetoo@proton.me>
2024-06-15 09:21:08 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 const app = express();
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`server is running on part ${port}`)
-})
+  console.log(`server is running on part ${port}`);
+});
