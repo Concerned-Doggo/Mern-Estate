5310cee25d02c860fe5e339b13730372ae87023a
concerned-doggo <codeformetoo@proton.me>
2024-06-25 11:33:57 +0530

    add image upload functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase.js";
 
 const profile = () => {
   const { currentUser } = useSelector((state) => state.user);
-  const [formData, setFormData] = useState();
+  const [formData, setFormData] = useState({});
+  const [file, setFile] = useState(null);
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(null);
+
+  const fileRef = useRef(null);
 
   const handleChange = (e) => {
     setFormData({
@@ -11,16 +23,71 @@ const profile = () => {
       [e.target.id]: e.target.value,
     });
   };
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadPercentage(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      },
+    );
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">profile</h1>
 
       <form className="flex flex-col gap-3">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
         <img
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2"
+          onClick={() => fileRef.current.click()}
         />
+
+        <p className="text-center text-sm">
+          {fileUploadError ? (
+            <span className="text-red-700">Error Image upload (image must be less than 2Mb)</span>
+          ) : uploadPercentage > 0 && uploadPercentage < 100 ? (
+            <span className="text-slate-700">
+              {`Uploading: ${uploadPercentage}%`}
+            </span>
+          ) : uploadPercentage === 100 ? (
+            <span className="text-green-700">
+              Image Uploaded Successfully!
+            </span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           placeholder={currentUser.username}
@@ -43,12 +110,14 @@ const profile = () => {
           id="password"
           onChange={handleChange}
         />
-                <button className="bg-slate-700 hover:opacity-95 disabled:opacity-80 text-white font-bold py-2 px-4 rounded-lg uppercase">Update</button>
+        <button className="bg-slate-700 hover:opacity-95 disabled:opacity-80 text-white font-bold py-2 px-4 rounded-lg uppercase">
+          Update
+        </button>
       </form>
-            <div className="flex justify-between mt-3">
-                <span className="text-red-700 cursor-pointer">Delete Account</span>
-                <span className="text-red-700 cursor-pointer">Sign Out</span>
-            </div>
+      <div className="flex justify-between mt-3">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
     </div>
   );
 };
