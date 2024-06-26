9c14575e2edaf3d086822ef53df0a911279dcdb3
concerned-doggo <codeformetoo@proton.me>
2024-06-26 12:04:52 +0530

    complete update user functionality

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -1,41 +1,50 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import {app} from '../firebase';
-import {useDispatch} from 'react-redux';
-import {signInSuccess } from '../redux/user/userSlice';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import {useNavigate} from 'react-router-dom';
 
 const OAuth = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const handleGoogleClick = async () => {
-        try {
-            // dispatch(signInStart());
+  const handleGoogleClick = async () => {
+    try {
+      // dispatch(signInStart());
 
-            const provider = new GoogleAuthProvider();
-            const auth = getAuth(app);
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-            const result = await signInWithPopup(auth, provider);
+      const result = await signInWithPopup(auth, provider);
 
-            const res = await fetch('/api/auth/google', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    username: result.user.displayName,
-                    email: result.user.email,
-                    photo: result.user.photoURL
-                })
-            })
-            const data = await res.json();
-            dispatch(signInSuccess(data));
-        
-        } catch (error) {
-            console.log('could not login with google', error);
-        }
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+
+      navigate("/");
+    } catch (error) {
+      console.log("could not login with google", error);
     }
+  };
   return (
-    <button type='button' onClick={handleGoogleClick} className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-90'>Continue with Google</button>
-  )
-}
+    <button
+      type="button"
+      onClick={handleGoogleClick}
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-90"
+    >
+      Continue with Google
+    </button>
+  );
+};
 
-export default OAuth
+export default OAuth;
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,125 +1,165 @@
 import { useState, useRef, useEffect } from "react";
-import { useSelector } from "react-redux";
+
+import { useSelector, useDispatch } from "react-redux";
 import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
+    updateUserStart,
+    updateUserFailure,
+    updateUserSuccess,
+} from "../redux/user/userSlice.js";
+
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase.js";
 
 const profile = () => {
-  const { currentUser } = useSelector((state) => state.user);
-  const [formData, setFormData] = useState({});
-  const [file, setFile] = useState(null);
-  const [uploadPercentage, setUploadPercentage] = useState(0);
-  const [fileUploadError, setFileUploadError] = useState(null);
+    const dispatch = useDispatch();
+    const { currentUser, loading, error } = useSelector((state) => state.user);
+    const [formData, setFormData] = useState({});
+    const [file, setFile] = useState(null);
+    const [uploadPercentage, setUploadPercentage] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(null);
+    const [updateSuccess, setUpdateSuccess] = useState(false);
 
-  const fileRef = useRef(null);
+    const fileRef = useRef(null);
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
-  };
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
 
-  useEffect(() => {
-    if (file) {
-      handleFileUpload(file);
-    }
-  }, [file]);
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
 
-  const handleFileUpload = (file) => {
-    const storage = getStorage(app);
-    const fileName = new Date().getTime() + file.name;
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setUploadPercentage(Math.round(progress));
-      },
-      (error) => {
-        setFileUploadError(error);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setFormData({ ...formData, avatar: downloadURL });
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setUploadPercentage(Math.round(progress));
+            },
+            (error) => {
+                setFileUploadError(error);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setFormData({ ...formData, avatar: downloadURL });
+                });
+            },
+        );
+    };
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
         });
-      },
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            dispatch(updateUserStart());
+            const res = await fetch(`/api/user/update/${currentUser._id}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+
+
+            if (data.success === false) {
+                dispatch(updateUserFailure(data.message));
+                return;
+            }
+
+            dispatch(updateUserSuccess(data));
+            setUpdateSuccess(true);
+        } catch (error) {
+            dispatch(updateUserFailure(error.message));
+        }
+    };
+    return (
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center font-semibold my-7">profile</h1>
+
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+                <input
+                    type="file"
+                    ref={fileRef}
+                    hidden
+                    accept="image/*"
+                    onChange={(e) => setFile(e.target.files[0])}
+                />
+                <img
+                    src={formData.avatar || currentUser.avatar}
+                    alt="profile"
+                    className="w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2"
+                    onClick={() => fileRef.current.click()}
+                />
+
+                <p className="text-center text-sm">
+                    {fileUploadError ? (
+                        <span className="text-red-700">
+                            Error Image upload (image must be less than 2Mb)
+                        </span>
+                    ) : uploadPercentage > 0 && uploadPercentage < 100 ? (
+                        <span className="text-slate-700">
+                            {`Uploading: ${uploadPercentage}%`}
+                        </span>
+                    ) : uploadPercentage === 100 ? (
+                        <span className="text-green-700">Image Uploaded Successfully!</span>
+                    ) : (
+                        ""
+                    )}
+                </p>
+                <input
+                    defaultValue={currentUser.username}
+                    type="text"
+                    className="border rounded-lg p-3"
+                    id="username"
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="email"
+                    defaultValue={currentUser.email}
+                    className="border rounded-lg p-3"
+                    id="email"
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    className="border rounded-lg p-3"
+                    id="password"
+                    onChange={handleChange}
+                />
+                <button
+                    disabled={loading}
+                    className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+                >
+                    {loading ? 'Loading...' : 'Update'}
+                    </button>
+            </form>
+            <div className="flex justify-between mt-3">
+                <span className="text-red-700 cursor-pointer">Delete Account</span>
+                <span className="text-red-700 cursor-pointer">Sign Out</span>
+            </div>
+            <p className="text-red-700 mt-5">{error ? error : ""}</p>
+            <p className="text-green-700 mt-5">
+                {updateSuccess ? "User is updated successfully!" : ""}
+            </p>
+        </div>
     );
-  };
-
-  return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">profile</h1>
-
-      <form className="flex flex-col gap-3">
-        <input
-          type="file"
-          ref={fileRef}
-          hidden
-          accept="image/*"
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-        <img
-          src={formData.avatar || currentUser.avatar}
-          alt="profile"
-          className="w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2"
-          onClick={() => fileRef.current.click()}
-        />
-
-        <p className="text-center text-sm">
-          {fileUploadError ? (
-            <span className="text-red-700">Error Image upload (image must be less than 2Mb)</span>
-          ) : uploadPercentage > 0 && uploadPercentage < 100 ? (
-            <span className="text-slate-700">
-              {`Uploading: ${uploadPercentage}%`}
-            </span>
-          ) : uploadPercentage === 100 ? (
-            <span className="text-green-700">
-              Image Uploaded Successfully!
-            </span>
-          ) : (
-            ""
-          )}
-        </p>
-        <input
-          type="text"
-          placeholder={currentUser.username}
-          className="border rounded-lg p-3"
-          id="username"
-          onChange={handleChange}
-        />
-
-        <input
-          type="email"
-          placeholder={currentUser.email}
-          className="border rounded-lg p-3"
-          id="email"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          className="border rounded-lg p-3"
-          id="password"
-          onChange={handleChange}
-        />
-        <button className="bg-slate-700 hover:opacity-95 disabled:opacity-80 text-white font-bold py-2 px-4 rounded-lg uppercase">
-          Update
-        </button>
-      </form>
-      <div className="flex justify-between mt-3">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
-      </div>
-    </div>
-  );
 };
 
 export default profile;
